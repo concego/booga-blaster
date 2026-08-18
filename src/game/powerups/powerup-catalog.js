c390c380c00e806e75aabf95596b072529f6652c
Anderson Carvalho | Eu Concego Jogar <euconcego@gmail.com>
2026-08-18 20:01:03 -0300

    feat: add bad news powerup

--- src/game/powerups/powerup-catalog.js
+++ src/game/powerups/powerup-catalog.js
@@ -3,7 +3,8 @@ export const POWERUP_TYPES = Object.freeze({
   GHOST_POTION: "ghost-potion",
   SALAMANDER: "salamander",
   MOLE: "mole",
-  SUPER_STRENGTH: "super-strength"
+  SUPER_STRENGTH: "super-strength",
+  BAD_NEWS: "bad-news"
 });
 
 export const POWERUPS = Object.freeze({
@@ -11,7 +12,8 @@ export const POWERUPS = Object.freeze({
   [POWERUP_TYPES.GHOST_POTION]: Object.freeze({ name: "Poção Fantasma", effect: "ghost", temporary: true }),
   [POWERUP_TYPES.SALAMANDER]: Object.freeze({ name: "Salamandra", effect: "fire-immunity", temporary: true }),
   [POWERUP_TYPES.MOLE]: Object.freeze({ name: "Toupeira", effect: "earth-immunity", temporary: true }),
-  [POWERUP_TYPES.SUPER_STRENGTH]: Object.freeze({ name: "Super Força", effect: "throw-range", temporary: true })
+  [POWERUP_TYPES.SUPER_STRENGTH]: Object.freeze({ name: "Super Força", effect: "throw-range", temporary: true }),
+  [POWERUP_TYPES.BAD_NEWS]: Object.freeze({ name: "Notícia ruim", effect: "bad-news", temporary: true, duration: 2 })
 });
 
 export const getPowerup = (type) => POWERUPS[type] || null;
