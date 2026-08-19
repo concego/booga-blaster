3d76452b970ef4274cf6d58feb98cfb8c863ed94
Anderson Carvalho | Eu Concego Jogar <euconcego@gmail.com>
2026-08-18 21:38:55 -0300

    fix: remove generator startup error

--- src/core/turn-engine.js
+++ src/core/turn-engine.js
@@ -1,6 +1,6 @@
-import { processEnemyTurn } from "../game/combat/enemy-engine.js?v=svg-test-39";
-import { resolveProjectiles } from "../game/spells/projectile-engine.js?v=svg-test-39";
-import { processFlameZones } from "../game/spells/zone-engine.js?v=svg-test-39";
+import { processEnemyTurn } from "../game/combat/enemy-engine.js?v=svg-test-43";
+import { resolveProjectiles } from "../game/spells/projectile-engine.js?v=svg-test-43";
+import { processFlameZones } from "../game/spells/zone-engine.js?v=svg-test-43";
 
 const hasBadNews = (state) => state.effects.some((effect) => effect.effect === "bad-news");
 
