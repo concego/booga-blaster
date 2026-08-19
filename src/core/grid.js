60412d34ab49de35e172e8013278c3cf86bf88fb
Anderson Carvalho | Eu Concego Jogar <euconcego@gmail.com>
2026-08-18 21:38:54 -0300

    fix: remove generator startup error

--- src/core/grid.js
+++ src/core/grid.js
@@ -1,4 +1,4 @@
-import { getDirection } from "./directions.js?v=svg-test-39";
+import { getDirection } from "./directions.js?v=svg-test-43";
 
 export const getBlockAt = (state, x, y) => {
   if (!state.grid.blocks) return null;
