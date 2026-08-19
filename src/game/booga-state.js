56e8469aafda8b496c0737a960562e1782500092
Anderson Carvalho | Eu Concego Jogar <euconcego@gmail.com>
2026-08-18 22:29:19 -0300

    fix: publish coherent module versions

--- src/game/booga-state.js
+++ src/game/booga-state.js
@@ -1,8 +1,8 @@
-import { POWERUP_SOURCES } from "./powerups/powerup-sources.js?v=svg-test-44";
-import { POWERUP_TYPES } from "./powerups/powerup-catalog.js?v=svg-test-44";
-import { createPowerupItem } from "./powerups/powerup-sources.js?v=svg-test-44";
-import { createHeartItem } from "./collectibles/heart-items.js?v=svg-test-44";
-import { generateLevel } from "./level-generator.js?v=svg-test-44";
+import { POWERUP_SOURCES } from "./powerups/powerup-sources.js?v=svg-test-46";
+import { POWERUP_TYPES } from "./powerups/powerup-catalog.js?v=svg-test-46";
+import { createPowerupItem } from "./powerups/powerup-sources.js?v=svg-test-46";
+import { createHeartItem } from "./collectibles/heart-items.js?v=svg-test-46";
+import { generateLevel } from "./level-generator.js?v=svg-test-46";
 
 const ELEMENT_NAMES = { fire: "Fogo", water: "Água", earth: "Terra", air: "Ar" };
 
