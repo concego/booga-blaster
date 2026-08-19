653a1e282c28d20db2d4049ea5bb9a00861700d5
Anderson Carvalho | Eu Concego Jogar <euconcego@gmail.com>
2026-08-18 21:38:51 -0300

    fix: remove generator startup error

--- src/game/spells/projectile-engine.js
+++ src/game/spells/projectile-engine.js
@@ -1,11 +1,11 @@
-import { getSpell } from "./spell-catalog.js?v=svg-test-39";
-import { blastCells } from "./blast.js?v=svg-test-39";
-import { coneCells, upsertZone } from "./area-effects.js?v=svg-test-39";
-import { pushEnemies, throwStones } from "./instant-effects.js?v=svg-test-39";
-import { revealBlockContents } from "../powerups/powerup-reveal.js?v=svg-test-39";
-import { damageEnemyAt, CONTACT_DAMAGE } from "../combat/damage.js?v=svg-test-39";
-import { damagePlayer } from "../combat/player-damage.js?v=svg-test-39";
-import { getBlockAt } from "../../core/grid.js?v=svg-test-39";
+import { getSpell } from "./spell-catalog.js?v=svg-test-43";
+import { blastCells } from "./blast.js?v=svg-test-43";
+import { coneCells, upsertZone } from "./area-effects.js?v=svg-test-43";
+import { pushEnemies, throwStones } from "./instant-effects.js?v=svg-test-43";
+import { revealBlockContents } from "../powerups/powerup-reveal.js?v=svg-test-43";
+import { damageEnemyAt, CONTACT_DAMAGE } from "../combat/damage.js?v=svg-test-43";
+import { damagePlayer } from "../combat/player-damage.js?v=svg-test-43";
+import { getBlockAt } from "../../core/grid.js?v=svg-test-43";
 
 const destroyBlocks = (state, cells, element) => {
   let destroyed = 0;
