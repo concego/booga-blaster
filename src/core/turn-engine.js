271119bd2d84192e4114fda52fa781b6a9878288
Anderson Carvalho | Eu Concego Jogar <euconcego@gmail.com>
2026-08-20 00:06:10 -0300

    chore: refresh special arena modules

--- src/core/turn-engine.js
+++ src/core/turn-engine.js
@@ -1,6 +1,6 @@
-import { processEnemyTurn } from "../game/combat/enemy-engine.js?v=svg-test-69";
-import { resolveProjectiles } from "../game/spells/projectile-engine.js?v=svg-test-69";
-import { processFlameZones } from "../game/spells/zone-engine.js?v=svg-test-69";
+import { processEnemyTurn } from "../game/combat/enemy-engine.js?v=svg-test-70";
+import { resolveProjectiles } from "../game/spells/projectile-engine.js?v=svg-test-70";
+import { processFlameZones } from "../game/spells/zone-engine.js?v=svg-test-70";
 
 const hasBadNews = (state) => state.effects.some((effect) => effect.effect === "bad-news");
 
