8bd2181a0649da1ab94d69a4f63e3d772e23f979
Anderson Carvalho | Eu Concego Jogar <euconcego@gmail.com>
2026-08-20 10:47:37 -0300

    test: add forest campaign phase transition

--- src/game/spells/zone-engine.js
+++ src/game/spells/zone-engine.js
@@ -1,4 +1,4 @@
-import { damageEnemyAt } from "../combat/damage.js?v=svg-test-75";
+import { damageEnemyAt } from "../combat/damage.js?v=svg-test-76";
 
 export const processFlameZones = (state) => {
   const events = [];
