e5a5604ab54ed82b62d0d534515bb56d097fc263
Anderson Carvalho | Eu Concego Jogar <euconcego@gmail.com>
2026-08-20 00:44:02 -0300

    feat: scale phase difficulty and Boss cadence

--- src/game/level-generator.js
+++ src/game/level-generator.js
@@ -1,4 +1,4 @@
-import { COMMON_ENEMIES, BIOME_ENEMIES, getPhaseContent } from "./content-catalog.js?v=svg-test-54";
+import { COMMON_ENEMIES, BIOME_ENEMIES, getPhaseContent } from "./content-catalog.js?v=svg-test-57";
 
 const WIDTH = 9;
 const HEIGHT = 5;
