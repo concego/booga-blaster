45e37bb4a7055d85a49cf85f3dad4fec851c3a2f
Anderson Carvalho | Eu Concego Jogar <euconcego@gmail.com>
2026-08-20 11:07:03 -0300

    test: auto-advance forest campaign phases

--- src/game/combat/damage.js
+++ src/game/combat/damage.js
@@ -1,5 +1,5 @@
-import { POWERUP_SOURCES, createPowerupItem } from "../powerups/powerup-sources.js?v=svg-test-76";
-import { createHeartItem } from "../collectibles/heart-items.js?v=svg-test-76";
+import { POWERUP_SOURCES, createPowerupItem } from "../powerups/powerup-sources.js?v=svg-test-77";
+import { createHeartItem } from "../collectibles/heart-items.js?v=svg-test-77";
 
 export const CONTACT_DAMAGE = 1;
 export const STONE_DAMAGE = 1;
