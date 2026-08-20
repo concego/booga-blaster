366e2e06d657fdc4be5d04177d4ba84004aa976a
Anderson Carvalho | Eu Concego Jogar <euconcego@gmail.com>
2026-08-20 11:38:28 -0300

    feat: complete powerup effects

--- src/game/special-arena.js
+++ src/game/special-arena.js
@@ -1,4 +1,4 @@
-import { getSpecialEnemyForPhase } from "./special-enemies.js?v=svg-test-78";
+import { getSpecialEnemyForPhase } from "./special-enemies.js?v=svg-test-79";
 
 const ARENA_WIDTH = 9;
 const ARENA_HEIGHT = 5;
