bfbab31b5d4f4e64b89b9e1b7334c5286ab0e028
Anderson Carvalho | Eu Concego Jogar <euconcego@gmail.com>
2026-08-20 14:37:16 -0300

    feat: add Bravo cover and potion escape

--- src/game/spells/spell-engine.js
+++ src/game/spells/spell-engine.js
@@ -1,7 +1,7 @@
-import { advanceTurn } from "../../core/turn-engine.js?v=svg-test-82";
-import { getDirection } from "../../core/directions.js?v=svg-test-82";
-import { getSpell } from "./spell-catalog.js?v=svg-test-82";
-import { findContactCell } from "./contact.js?v=svg-test-82";
+import { advanceTurn } from "../../core/turn-engine.js?v=svg-test-83";
+import { getDirection } from "../../core/directions.js?v=svg-test-83";
+import { getSpell } from "./spell-catalog.js?v=svg-test-83";
+import { findContactCell } from "./contact.js?v=svg-test-83";
 
 const hasEffect = (state, effect) => state.effects.some((item) => item.effect === effect);
 const getRange = (state, baseRange) => (hasEffect(state, "throw-range") ? baseRange + 1 : baseRange);
