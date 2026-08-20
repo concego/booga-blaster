cb557a73a340b5f3d3a5af96b925181ae0a2893d
Anderson Carvalho | Eu Concego Jogar <euconcego@gmail.com>
2026-08-19 22:44:43 -0300

    feat: expose enemy names for sound mapping

--- src/core/turn-engine.js
+++ src/core/turn-engine.js
@@ -1,6 +1,6 @@
-import { processEnemyTurn } from "../game/combat/enemy-engine.js?v=svg-test-43";
-import { resolveProjectiles } from "../game/spells/projectile-engine.js?v=svg-test-43";
-import { processFlameZones } from "../game/spells/zone-engine.js?v=svg-test-43";
+import { processEnemyTurn } from "../game/combat/enemy-engine.js?v=svg-test-67";
+import { resolveProjectiles } from "../game/spells/projectile-engine.js?v=svg-test-67";
+import { processFlameZones } from "../game/spells/zone-engine.js?v=svg-test-67";
 
 const hasBadNews = (state) => state.effects.some((effect) => effect.effect === "bad-news");
 
