87a4d9a7c3473a5ae3925ea0de3ce95512e01ad8
Anderson Carvalho | Eu Concego Jogar <euconcego@gmail.com>
2026-08-20 00:06:09 -0300

    chore: refresh special arena modules

--- src/game/special-arena.js
+++ src/game/special-arena.js
@@ -1,4 +1,4 @@
-import { getSpecialEnemyForPhase } from "./special-enemies.js?v=svg-test-69";
+import { getSpecialEnemyForPhase } from "./special-enemies.js?v=svg-test-70";
 
 const ARENA_WIDTH = 9;
 const ARENA_HEIGHT = 5;
