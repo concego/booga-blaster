f392ce0a170641ff662e7ba567ca8d0c673f83a8
Anderson Carvalho | Eu Concego Jogar <euconcego@gmail.com>
2026-08-20 14:12:45 -0300

    fix: simplify combat feedback

--- src/core/turn-engine.js
+++ src/core/turn-engine.js
@@ -1,6 +1,6 @@
-import { processEnemyTurn } from "../game/combat/enemy-engine.js?v=svg-test-79";
-import { resolveProjectiles } from "../game/spells/projectile-engine.js?v=svg-test-79";
-import { processFlameZones } from "../game/spells/zone-engine.js?v=svg-test-79";
+import { processEnemyTurn } from "../game/combat/enemy-engine.js?v=svg-test-80";
+import { resolveProjectiles } from "../game/spells/projectile-engine.js?v=svg-test-80";
+import { processFlameZones } from "../game/spells/zone-engine.js?v=svg-test-80";
 
 const hasBadNews = (state) => state.effects.some((effect) => effect.effect === "bad-news");
 
