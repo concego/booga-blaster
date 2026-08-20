d5a32b2048ceb1783c0fe9674b272ac103b140c6
Anderson Carvalho | Eu Concego Jogar <euconcego@gmail.com>
2026-08-20 10:47:28 -0300

    test: add forest campaign phase transition

--- src/game/level-generator.js
+++ src/game/level-generator.js
@@ -1,4 +1,4 @@
-import { COMMON_ENEMIES, BIOME_ENEMIES, getPhaseContent } from "./content-catalog.js?v=svg-test-75";
+import { COMMON_ENEMIES, BIOME_ENEMIES, getPhaseContent } from "./content-catalog.js?v=svg-test-76";
 
 const WIDTH = 9;
 const HEIGHT = 5;
