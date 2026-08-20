a3f220e91ba922cc2be8f7a88ec05afc30baa577
Anderson Carvalho | Eu Concego Jogar <euconcego@gmail.com>
2026-08-20 00:28:51 -0300

    fix: refine Troll path behavior

--- src/game/booga-actions.js
+++ src/game/booga-actions.js
@@ -1,10 +1,10 @@
-import { tryMovePlayer } from "../core/grid.js?v=svg-test-70";
-import { advanceTurn, getTurnEvents } from "../core/turn-engine.js?v=svg-test-70";
-import { scanState, getAdjacentFindings } from "./scan/scan-state.js?v=svg-test-70";
-import { castSpell } from "./spells/spell-engine.js?v=svg-test-70";
-import { getSpell } from "./spells/spell-catalog.js?v=svg-test-70";
-import { collectAtCell } from "./powerups/powerup-system.js?v=svg-test-70";
-import { enterSpecialArena } from "./special-arena.js?v=svg-test-70";
+import { tryMovePlayer } from "../core/grid.js?v=svg-test-71";
+import { advanceTurn, getTurnEvents } from "../core/turn-engine.js?v=svg-test-71";
+import { scanState, getAdjacentFindings } from "./scan/scan-state.js?v=svg-test-71";
+import { castSpell } from "./spells/spell-engine.js?v=svg-test-71";
+import { getSpell } from "./spells/spell-catalog.js?v=svg-test-71";
+import { collectAtCell } from "./powerups/powerup-system.js?v=svg-test-71";
+import { enterSpecialArena } from "./special-arena.js?v=svg-test-71";
 
 const appendTurnEvents = (state, message) => {
   const events = getTurnEvents(state);
