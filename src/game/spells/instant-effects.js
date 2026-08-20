a6799aca8c1f03691c1514c0a956835c99d521e4
Anderson Carvalho | Eu Concego Jogar <euconcego@gmail.com>
2026-08-20 11:38:30 -0300

    feat: complete powerup effects

--- src/game/spells/instant-effects.js
+++ src/game/spells/instant-effects.js
@@ -1,5 +1,5 @@
-import { isBlocked } from "../../core/grid.js?v=svg-test-78";
-import { STONE_DAMAGE, damageEnemyAt } from "../combat/damage.js?v=svg-test-78";
+import { isBlocked } from "../../core/grid.js?v=svg-test-79";
+import { STONE_DAMAGE, damageEnemyAt } from "../combat/damage.js?v=svg-test-79";
 
 const enemyAt = (state, cell) => state.enemies.find((enemy) => enemy.x === cell.x && enemy.y === cell.y);
 
