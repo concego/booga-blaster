e056f779222407f1815861475445aa16fc78c297
Anderson Carvalho | Eu Concego Jogar <euconcego@gmail.com>
2026-08-20 10:47:26 -0300

    test: add forest campaign phase transition

--- src/game/booga-actions.js
+++ src/game/booga-actions.js
@@ -1,10 +1,10 @@
-import { tryMovePlayer } from "../core/grid.js?v=svg-test-75";
-import { advanceTurn, getTurnEvents } from "../core/turn-engine.js?v=svg-test-75";
-import { scanState, getAdjacentFindings } from "./scan/scan-state.js?v=svg-test-75";
-import { castSpell } from "./spells/spell-engine.js?v=svg-test-75";
-import { getSpell } from "./spells/spell-catalog.js?v=svg-test-75";
-import { collectAtCell } from "./powerups/powerup-system.js?v=svg-test-75";
-import { enterSpecialArena } from "./special-arena.js?v=svg-test-75";
+import { tryMovePlayer } from "../core/grid.js?v=svg-test-76";
+import { advanceTurn, getTurnEvents } from "../core/turn-engine.js?v=svg-test-76";
+import { scanState, getAdjacentFindings } from "./scan/scan-state.js?v=svg-test-76";
+import { castSpell } from "./spells/spell-engine.js?v=svg-test-76";
+import { getSpell } from "./spells/spell-catalog.js?v=svg-test-76";
+import { collectAtCell } from "./powerups/powerup-system.js?v=svg-test-76";
+import { enterSpecialArena } from "./special-arena.js?v=svg-test-76";
 
 const appendTurnEvents = (state, message) => {
   const events = getTurnEvents(state);
